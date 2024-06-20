19ff89f41d8cbdd324cff5394cc8037dc77403d0
Ad-MY <9391@holbertonstudents.com>
2024-06-20 13:29:10 +0000

    _putchar

--- functions_nested_loops/0-putchar.c
+++ functions_nested_loops/0-putchar.c
@@ -2,12 +2,12 @@
 #include <main.h>
 
 /**
- * main - prints the string "Holberton" from a character array.
+ * main - prints the string  from a character array.
  * Return: 0 on success.
  */
 int main(void)
 {
-	char c[] = "Holberton";
+	
 	int i;
 
 	for (i = 0; i < 9; i++)
