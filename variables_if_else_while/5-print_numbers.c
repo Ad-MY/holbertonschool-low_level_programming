9e22253704a84aa1c85ed686937fbb53e6ecc4f9
Ad-MY <9391@holbertonstudents.com>
2024-06-20 09:59:28 +0000

    Numbers

--- variables_if_else_while/5-print_numbers.c
+++ variables_if_else_while/5-print_numbers.c
@@ -1,21 +1,18 @@
 #include <stdio.h>
 
 /**
- * main - Prints the alphabet in lowercase, and then in uppercase.
+ * main - Prints all single digit numbers of base 10 starting from 0.
  *
  * Return: Always 0.
  */
 int main(void)
 {
-        char number;
+        int numbers;
 
-        for (letter = '0'; number <= '10'; number++)
-                putchar(number);
+	for (numbers = 0; numbers < 10; numbers++)
+		printf("%d", numbers);
 
-        for (number = '0'; number <= '10'; number++)
-                putchar(number);
+	printf("\n");
 
-        putchar('\n');
-
-        return (0);
-}
+	return (0);
+{	
