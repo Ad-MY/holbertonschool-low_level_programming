5150127251ab4d24e38ffcf1dcb39d24477ad8b3
Ad-MY <9391@holbertonstudents.com>
2024-06-20 13:20:41 +0000

    _putchar

--- functions_nested_loops/0-putchar.c
+++ functions_nested_loops/0-putchar.c
@@ -1,20 +1,20 @@
 #include "main.h"
+
 /**
- * main - Entry point
- *
- * Description - print _putchar
- *
- * Return: always 0
- */
+*main - function prints text as output
+*
+*Return:return 0
+*/
 int main(void)
 {
-	char c[] = "_putchar";
+	char school[10] = "_putchar";
+
 	int i;
 
 	for (i = 0; i < 8; i++)
 	{
-		_putchar(c[i]);
+		_putchar(school[i]);
 	}
-	_putchar('\n');
+	_putchar(10);
 	return (0);
 }
